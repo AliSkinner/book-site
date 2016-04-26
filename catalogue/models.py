9ae0505823234977cabbdf74b91576c70e77d4cd
alistair.skinner <alistair@thepetspyjamas.com>
2016-04-26 11:48:16 +0100

    auto populate slugs in admin & edit to models

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
-    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
-    date_of_death = models.DateField(auto_now=False, auto_now_add=False)
-    nationality = models.DateField(auto_now=False, auto_now_add=False)
-    bio = models.TextField()
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    date_of_death = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    nationality = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
     slug = models.SlugField(max_length=255)
-    url = models.URLField(max_length=255, verbose_name="Extenal Link URL")
+    url = models.URLField(max_length=255, verbose_name="Extenal Link URL", blank=True)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.surname)
@@ -16,12 +16,12 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    description = models.TextField()
-    pub_date = models.DateField(auto_now=False, auto_now_add=False)
-    # image = models.ImageField(upload_to=None) 
-    isbn = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    pub_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    # image = models.ImageField(upload_to=None)
+    isbn = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(max_length=255)
-    url = models.URLField(max_length=255, verbose_name="Extenal Link URL")
+    url = models.URLField(max_length=255, verbose_name="Extenal Link URL", blank=True)
 
     def __str__(self):
         return self.title
